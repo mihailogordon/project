a9f71b71ec0b6fff7940543ea9d53a37f76fd1a1
mihailogordon <36379645+mihailogordon@users.noreply.github.com>
2018-08-31 18:58:16 +0200

    First commit

---
+++ db_definition.php
@@ -0,0 +1,47 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "doskocidb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE " . $dbname;
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+// Create connection to created Database
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "CREATE TABLE Doskocice (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+doskocica_initial VARCHAR(50),
+doskocica VARCHAR(3000),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/*Create required DB and Tables in it*/
+//include_once 'db_definition.php';
+?>
+<?php
+include_once "templates/header.php";
+?>
+
+<div class="text-input-holder">
+    <input type="text" placeholder="Search...">
+</div>
+<div class="input-buttons-holder">
+    <a href="#" class="button-search">Pretrazi</a>
+    <a href="#" class="button-search">Iz prve ruke</a>
+</div>
